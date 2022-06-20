a7d188baf475f99ddc2d23bd5e38989f41905891
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2022-06-20 17:44:44 -0500

    Fundamentos

---
+++ Fundamentos/HolaMundoJava/src/UserInput.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Mazo
+ */
+public class UserInput {
+    public static void main(String[] args) {
+        System.out.println("Escribe tu nombre: ");
+        
+        Scanner consola = new Scanner(System.in);
+        var nombreUsuario =  consola.nextLine();
+        System.out.println("usuario = " + nombreUsuario);
+        
+        System.out.println("Edad: ");
+        var edadUsuario = consola.nextInt();
+        System.out.println("edadUsuario = " + edadUsuario);
+        
+        System.out.println("Titulo del usuario:");
+        var tituloUsuario = consola.next();
+        System.out.println("tituloUsuario = " + tituloUsuario);
+        
+        System.out.println(tituloUsuario + ": " + nombreUsuario + ", " + edadUsuario);
+    }
+}
