1aeba96602807732e94922a13c95f2a93c062004
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2022-06-22 14:01:59 -0500

    Fundamentos

---
+++ Fundamentos/HolaMundoJava/src/Seccion_7/Persona.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Seccion_7;
+
+/**
+ *
+ * @author Mazo
+ */
+public class Persona {
+    public String nombre;
+    public String apellido;
+   
+    
+    public void desplegarInformacion(){
+        System.out.println("nombre = " + nombre);
+        System.out.println("apellido = " + apellido);
+    }
+    
+}
---
+++ Fundamentos/HolaMundoJava/src/Seccion_7/PruebaPersona.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Seccion_7;
+
+/**
+ *
+ * @author Mazo
+ */
+public class PruebaPersona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona persona1;
+        persona1 = new Persona();
+        persona1.nombre = "Sebastian";
+        persona1.apellido = "Mazo Velez";
+        persona1.desplegarInformacion();
+    }
+    
+    
+}
